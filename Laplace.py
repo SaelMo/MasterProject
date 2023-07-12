3a076d88582d48fefdd00112c478b8ec0324edbd
Saad el Morabit <70237218+SaelMo@users.noreply.github.com>
2023-07-12 21:33:02 +0200

    Add files via upload

--- Laplace.py
+++ Laplace.py
@@ -1,30 +1,37 @@
 '''Back of the Envelope estimate of a potential given a rectangular boundary.
 The code is written to be read as briefly as possible and to be easy to alter.
 '''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-Nx, Ny = 60, 200 # Meshgrid resolution
-Vu, Vd, Vs = 1000, 0, -2000 # Dirichlet Electrodes
+Nx, Ny = 30*5, 120*5 # Meshgrid resolution
+Vu, Vd, Vs = 1000, 0, -500 # Dirichlet Electrodes
 
-# Meshgrid, ambiguity at corners do not matter as they're ignored by the algorithm
-Potinit = np.zeros((Nx, Ny))
+# Meshgrid ambiguity at corners do not matter as they do not factor into the algorithm 
+Potinit = (Vu+Vd+2*Vs)/4*np.ones((Nx, Ny)) # Initiate with mean of boundaries for faster convergence
 Potinit[0, :], Potinit[-1, :] = Vu, Vd
 Potinit[:, 0], Potinit[:, -1] = Vs, Vs
+Pot = np.array(Potinit)
+
+# The more runs the better although a convergence condition would be better
+Runs = 5000
+for n in range(Runs):
+    PotBuffer = np.array(Potinit)
+    
+    # For every point on the interior the average of the nearest neighbours is taken
+    PotBuffer[1:Nx-1,1:Ny-1] = ((np.roll(Pot, 1, axis = 1)+np.roll(Pot, -1, axis = 1) + 
+    np.roll(Pot, 1, axis = 0)+np.roll(Pot, -1, axis = 0))/4)[1:Nx-1,1:Ny-1]
+    Pot = PotBuffer
+
 plt.imshow(Potinit)
 plt.colorbar()
 plt.show()
-Pot = np.array(Potinit)
-
-# the more runs the better, the reader can decide on a more sophisticated conditions themselves
-Runs = 100
-for n in range(Runs):
-    PotBuffer = np.array(Potinit)
-    for i in range(1, Nx-1):
-        for j in range(1, Ny-1):
-            PotBuffer[i, j] = (Pot[i-1, j]+Pot[i+1, j]+Pot[i, j-1]+Pot[i, j+1])/4
-    Pot = PotBuffer
-
-plt.imshow(Pot)
-plt.colorbar()
+plt.imshow(Pot, interpolation='nearest', extent=[-6, 6, -1.5, 1.5])
+cbar = plt.colorbar()
+cbar.set_label('Potential [V]', rotation=270)
+plt.xlabel("x [cm]")
+plt.ylabel("y [cm]")
+plt.show()
+plt.plot(Pot[int(Nx/2),:])
 plt.show()
